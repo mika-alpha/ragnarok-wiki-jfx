bb383fa51caf395efc2b370d6bacae8a9a89a2bb
anima-anemone <78612129+anima-anemone@users.noreply.github.com>
2021-06-11 10:06:22 -0500

    ## [0.1]
    - add the base main fxml.
    
    - add the 'add monster' fxml

--- src/io/github/anemone/anima/gui/Main.java
+++ src/io/github/anemone/anima/gui/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        primaryStage.setTitle("Ragnarok wiki database");
+        primaryStage.setScene(new Scene(root,1280, 720));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
